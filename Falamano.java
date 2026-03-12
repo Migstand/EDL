42a92de8968129241adb5896bd073fce4a9ffb6d
Migstand <mrsdahora@gmail.com>
2026-03-12 22:35:05 +0000

    Pequenos testes de java. Começando agora.

---
+++ Falamano.java
@@ -0,0 +1,10 @@
+public class Falamano{
+    public static void main(String[] args){
+
+        int Brother = 20;
+        float Cara = 15.5f;
+        float Amigos = Brother + Cara;
+        System.out.println("Fala manos !!!!");
+        System.out.printf("Temos " + Amigos +", tá ligado?");
+    }
+}
